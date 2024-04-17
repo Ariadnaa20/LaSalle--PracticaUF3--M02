0a2b8cb0608290beebb10aa1c9fb4a1934eb63f9
ariadna <ari.palau20@gmail.com>
2024-04-17 20:23:38 +0200

    plantejament

--- CreacióTaules.sql
+++ CreacióTaules.sql
@@ -2,9 +2,16 @@
 
 --1. Plantejament de l'estructura de la taula
 
-/**/
+/* Hem decidit estructurar la taula d'aquesta manera per diverses raons. En primer lloc, hem optat per utilitzar una clau primària autoincremental per a la columna id per proporcionar una identificació única per a cada registre de log. Això facilita la gestió de les dades i les operacions de consulta.
 
---1. Taula que rebrà les dades del fitxer
+En segon lloc, hem separar la data i l'hora en columnes independents (Fecha i Hora) perquè ens permet gestionar de manera més flexible les dades i facilita les consultes que necessiten filtrar per data, hora o intervals de temps específics.
+
+Quant a les columnes Sistema, Origen i Mensaje, les hem seleccionades per reflectir els components més rellevants dels registres de log. Utilitzar el tipus de dada VARCHAR per a aquestes columnes ens proporciona una estructura flexible i adaptable als tipus de dades que esperem emmagatzemar.
+
+Finalment, hem triat utilitzar el tipus de dada TEXT per a la columna Mensaje per acomodar missatges de log de longitud variable. Això ens permet emmagatzemar missatges de log de qualsevol mida sense preocupar-nos per restriccions de longitud.*/
+
+
+--2. Taula que rebrà les dades del fitxer
 
 USE DBPractica;
 
@@ -19,6 +26,7 @@ CREATE TABLE IF NOT EXISTS CarregarLogs (
     Mensaje VARCHAR(255),
     PRIMARY KEY (id)
 );
+
 
 
 --3. Codi encarregat de carregar aquest fitxer
